b6002fd605f5b0356c2c3a0e1bff68b2dc650f16
Duy <huynhduytam2210@gmail.com>
2021-05-03 00:06:27 +0700

    initial pro

--- src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import postsReducer from './posts/PostSlice'
-
-export default configureStore({
-    reducer: {
-        posts: postsReducer
-    }
-})
