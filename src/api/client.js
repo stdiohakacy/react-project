ef4f94e01dd4fe685c09884fb06124bc98eb304c
Duy <huynhduytam2210@gmail.com>
2021-05-03 11:35:36 +0700

    fake client call api

---
+++ src/api/client.js
@@ -0,0 +1,30 @@
+export async function client(endpoint, { body, ...customConfig } = {}) {
+  const headers = { "Content-Type": "application/json" };
+  const config = {
+    method: body ? "POST" : "GET",
+    ...customConfig,
+    headers: {
+      ...headers,
+      ...customConfig.headers,
+    },
+  };
+
+  if (body) config.body = JSON.stringify(body);
+  let data;
+  try {
+    const response = await window.fetch(endpoint, config);
+    data = await response.json();
+    if (response.ok) return data;
+    throw new Error(response.statusText);
+  } catch (error) {
+    console.error(error);
+    return Promise.reject(error.message ? error.message : data);
+  }
+}
+
+client.get = function (endpoint, customConfig = {}) {
+  return client(endpoint, { ...customConfig, method: "GET" });
+};
+client.post = function (endpoint, body, customConfig = {}) {
+  return client(endpoint, { ...customConfig, body });
+};
