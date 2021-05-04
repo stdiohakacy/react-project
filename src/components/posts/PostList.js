aa6cc58f07bd4ec0dcf89f6e744bdfa640fc45fc
Duy <huynhduytam2210@gmail.com>
2021-05-04 11:26:41 +0700

    initial proj

--- src/components/posts/PostList.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export const PostsList = () => {
-    return (
-        <div>Posts List</div>
-    )
-};
--- src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import postsReducer from "./posts/PostsSlice";
-
-export default configureStore({
-  reducer: {
-    posts: postsReducer,
-  },
-});
