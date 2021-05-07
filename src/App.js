5e4a81dadc31cf7f9a935db468c81e28fd5f8b16
Duy <huynhduytam2210@gmail.com>
2021-05-07 10:34:44 +0700

    edit post

--- src/App.js
+++ src/App.js
@@ -7,9 +7,10 @@ import {
 } from "react-router-dom";
 import { Navbar } from "./app/Navbar";
 import "./App.css";
-import { AddPostForm } from './features/posts/addPostForm'
+import { AddPostForm } from "./features/posts/addPostForm";
 import { PostList } from "./features/posts/postList";
 import { SinglePostPage } from "./features/posts/singlePostPage";
+import { EditPostForm } from "./features/posts/editPostForm";
 
 function App() {
   return (
@@ -28,6 +29,7 @@ function App() {
             )}
           />
           <Route exact path="/posts/:postId" component={SinglePostPage} />
+          <Route exact path="/editPost/:postId" component={EditPostForm} />
           <Redirect to="/" />
         </Switch>
       </div>
